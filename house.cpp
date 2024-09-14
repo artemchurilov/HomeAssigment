7d377b8b1cffe87323ad43285d94c463aa60989c
artem <artem@GopheR.myguest.virtualbox.org>
2024-09-15 00:08:19 +0300

    create hello world + hello <line> code

---
+++ house.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "f.h"
+
+using namespace std;
+
+string newname;
+
+int main(){
+	cout << "Hello, World!"<<endl;
+	cin >> newname;
+	cout << "Hello, " << newname<<"!"<<endl;
+}
