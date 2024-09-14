80a7e9c98185886c9c52ddfc6fb66e8f6741d6b2
artemchurilov <st130184@student.spbu.ru>
2024-09-15 00:45:13 +0300

    added while--loop()

--- house.cpp
+++ house.cpp
@@ -1,12 +1,20 @@
+/* Artem Churilov st130184@student.spbu.ru
+   first task "Hello, World! V. 2.0"
+*/
+
 #include <iostream>
 #include "f.h"
 
 using namespace std;
 
+int i = 42;
 extern string newline;
 
 int main(){
 	hello();
-	cin >> newline;
-	cout << "Hello, " << newline<<"!"<<endl;
+	while (i>-42){
+		cout << "What's your name?"<<endl;
+		cin >> newline;
+		cout << "Hello, " << newline<<"!"<<endl;
+	}
 }
