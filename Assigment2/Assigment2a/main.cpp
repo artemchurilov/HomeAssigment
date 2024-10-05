2226863372550262633bd3b2eb76c16d7cb821ec
artemchurilov <st130184@student.spbu.ru>
2024-10-06 00:03:15 +0300

    create code to read info from file and write it into other file using array, also add Makefile

--- Assigment2/Assigment2a/main.cpp
+++ Assigment2/Assigment2a/main.cpp
@@ -4,14 +4,19 @@
 
 int main()
 {
-    std::filesystem::path infilepath{"testfile.txt"};
+    int i = 0;
+    char savevar;
+    std::filesystem::path infilepath{"test.txt"};
     std::ifstream infile;
     std::ofstream outfile;
     infile.open(infilepath, std::ios::binary|std::ios::in);
     uintmax_t fsize = std::filesystem::file_size(infilepath);
-    std::filesystem::path outfilepath{"temppdf.pdf"};
+    char *buffer = new char[fsize];
+    infile.read((char *)buffer,fsize);
+    std::filesystem::path outfilepath{"final.txt"};
     outfile.open(outfilepath, std::ios::binary|std::ios::out);
-    std::cout <<fsize<<std::endl;
+    outfile.write((char*)buffer, fsize);
     infile.close();
     outfile.close();
+    delete [] buffer;
 }
